52abc75ad82e0467cf4bde6376e23f02e7f2d2d6
Benjamin Skirlo <benji@tur.ink>
2016-08-12 00:41:57 +0200

    login in browser with google works. not yet in emulator

--- app/pages/login/login.ts
+++ app/pages/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFire, AuthProviders,AuthMethods } from "angularfire2/angularfire2";
 
 /*
   Generated class for the LoginPage page.
@@ -12,8 +13,17 @@ import { NavController } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(private navCtrl: NavController) {
-
+  constructor(private navCtrl: NavController,
+              private fire:AngularFire) {
+    this.fire.auth.subscribe( auth=>console.log(auth));
   }
+
+  private login() {
+    this.fire.auth.login({
+      provider: AuthProviders.Google,
+      method: AuthMethods.Popup,
+    })
+  }
+
 
 }
