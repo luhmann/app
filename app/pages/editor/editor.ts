f94328939f2d27cf9245839dee36402c406c741c
Benjamin Skirlo <benji@tur.ink>
2016-08-15 19:13:54 +0200

    Editor Tools in their own files

--- app/pages/editor/editor.ts
+++ app/pages/editor/editor.ts
@@ -1,20 +1,22 @@
 import { Component, HostListener } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import * as paper        from "paper";
-import {Size}            from "paper";
-import {Point}           from "paper";
-import {AngularFire}     from "angularfire2/angularfire2";
-import defaultFirebase   from "angularfire2/angularfire2";
-import {NavParams}       from "ionic-angular/index";
-import {Composition}     from "../../lib/Composition";
-import {FirebaseListObservable} from "angularfire2/angularfire2";
-import {Path} from "paper";
-import {FirebaseObjectObservable} from "angularfire2/angularfire2";
-import {Item} from "paper";
-import {FirebaseOperation} from "angularfire2/es6/utils/firebase_list_observable";
-import Reference = firebase.database.Reference;
+import { NavController }           from 'ionic-angular';
+import * as paper                  from "paper";
+import {Size}                      from "paper";
+import {Point}                     from "paper";
+import {AngularFire}               from "angularfire2/angularfire2";
+import defaultFirebase             from "angularfire2/angularfire2";
+import {NavParams}                 from "ionic-angular/index";
+import {Composition}               from "../../lib/Composition";
+import {FirebaseListObservable}    from "angularfire2/angularfire2";
+import {FirebaseObjectObservable}  from "angularfire2/angularfire2";
+import {FirebaseOperation}         from "angularfire2/es6/utils/firebase_list_observable";
+import {Path}                      from "paper";
+import {Item}                      from "paper";
+import {Tool}                      from "paper";
+import {PenTool}                   from "./tools/PenTool";
+import {FirePath}                  from "./tools/FirePath";
+import Reference    = firebase.database.Reference;
 import DataSnapshot = firebase.database.DataSnapshot;
-import {Tool} from "paper";
 
 
 /*
@@ -25,10 +27,7 @@ import {Tool} from "paper";
 */
 
 
-interface FirePath {
-  id   :number;
-  json :string;
-}
+
 
 @Component({
   templateUrl: 'build/pages/editor/editor.html',
@@ -39,8 +38,6 @@ export class EditorPage{
   private loaded      :boolean;
   private paths       :FirebaseListObservable<any[]>
   private composition :FirebaseListObservable<Composition>
-  private keyToPaths  :{[key:string]: {path :Path, id  :number, reference :Reference}};
-  private idToKey     :{[id :number]: {path :Path, key :string, reference :Reference}};
 
   constructor(private navCtrl :NavController,
               private fire    :AngularFire,
@@ -49,8 +46,6 @@ export class EditorPage{
     let key = params.get("id");
 
     this.paths = fire.database.list('/paths/' + key);
-    this.keyToPaths = {};
-    this.idToKey    = {};
   }
 
   ionViewDidEnter() {
@@ -64,16 +59,14 @@ export class EditorPage{
     let _canvas :HTMLCanvasElement = canvas as HTMLCanvasElement;
     let width  :number = _canvas.clientWidth;
     let height :number = _canvas.clientHeight;
+    let reference :Reference = this.paths._ref as Reference;
 
-
-    //debugger;
-    console.log(paper, _canvas);
 
     paper.view.viewSize = new paper.Size(new paper.Point(width,height));
-    paper.tool = new paper.Tool();
-    paper.tool.onMouseUp   = this.onMouseUp  .bind(this);
-    paper.tool.onMouseDown = this.onMouseDown.bind(this);
-    paper.tool.onMouseDrag = this.onMouseDrag.bind(this);
+
+
+    var penTool = new PenTool(this.paths);
+
 
     this.textItem = new paper.PointText({
       content  : 'Click and drag to draw a line.',
@@ -84,15 +77,14 @@ export class EditorPage{
 
     //this.paths.subscribe(this.handleSubscribtion);
 
-    let reference :Reference = this.paths._ref as Reference;
     reference.on('child_added',   this.handleAddedChildren  .bind(this));
     reference.on('child_changed', this.handleChangedChildren.bind(this));
 
   }
 
-  private handleAddedChildren(data :DataSnapshot){
+  private handleAddedChildren(data :DataSnapshot)
+  {
     let firePath = data.val() as FirePath;
-    let key      = data.key;
 
 
     let item = paper.project.getItem(firePath.id) || new Path();
@@ -101,14 +93,14 @@ export class EditorPage{
 
   }
 
-  private handleChangedChildren(data :DataSnapshot){
+  private handleChangedChildren(data :DataSnapshot)
+  {
     let firePath = data.val() as FirePath;
-    let key      = data.key;
 
-    let path :Path;
+    let path:Path;
 
-    if(!this.path || this.path.id != firePath.id) {
-      path = paper.project.getItem({id:firePath.id}) as Path;
+    if (!this.path || this.path.id != firePath.id) {
+      path = paper.project.getItem({id: firePath.id}) as Path;
       if (path) {
         path.removeChildren();
       }
@@ -121,81 +113,8 @@ export class EditorPage{
 
   }
 
-  private onMouseDown(event :paper.ToolEvent) {
-    // If we produced a path before, deselect it:
-    if (this.path) {
-      this.path.selected = false;
-    }
-
-    this.path = new Path({
-      segments: [event.point],
-      strokeColor: 'black',
-      // Select the path, so we can see its segment points:
-      fullySelected: true
-    });
-
-    let json = this.path.exportJSON();
-    let id   = this.path.id;
-
-    var firePath :FirePath = {json:json, id:id};
-    this.paths.push(firePath).then(
-      (response: any)=>{
-        let reference :Reference = response as Reference;
-        let key       :string    = reference.key;
-        let path      :Path      = this.path;
-        this.keyToPaths[key] = {id : id,  path: path, reference: reference};
-        this.idToKey   [id]  = {key: key, path: path, reference: reference};
-        console.log(response);
-      }
-    )
-  }
 
 
-  // While the user drags the mouse, points are added to the path
-  // at the position of the mouse:
-  private onMouseDrag(event :paper.ToolEvent) {
-    this.path.add(event.point);
 
-    // Update the content of the text item to show how many
-    // segments it has:
-    this.textItem.content = 'Segment count: ' + this.path.segments.length;
-
-    //var json = this.path.exportJSON();
-    this.savePathChanges();
-
-  }
-
-  private savePathChanges() {
-    let id = this.path.id;
-    let idToKey = this.idToKey[id];
-
-    if (idToKey) {
-      let key = idToKey.key;
-      let json = this.path.exportJSON();
-
-      let firePath:FirePath = {json: json, id: id};
-
-      console.log(key);
-      this.keyToPaths[key].reference.set(firePath);
-    }
-  };
-
-// When the mouse is released, we simplify the path:
-  private onMouseUp(event:paper.ToolEvent) {
-    let segmentCount = this.path.segments.length;
-
-    // When the mouse is released, simplify it:
-    this.path.simplify(10);
-
-    // Select the path, so we can see its segments:
-    this.path.fullySelected = true;
-
-    let newSegmentCount = this.path.segments.length;
-    let difference = segmentCount - newSegmentCount;
-    let percentage = 100 - Math.round(newSegmentCount / segmentCount * 100);
-    this.textItem.content = difference + ' of the ' + segmentCount + ' segments were removed. Saving ' + percentage + '%';
-    this.path.selected = false;
-    this.savePathChanges();
-  }
 
 }
---
+++ app/pages/editor/tools/FirePath.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by benjaminskirlo1 on 15.08.16.
+ */
+export interface FirePath {
+  id   :number;
+  json :string;
+}
